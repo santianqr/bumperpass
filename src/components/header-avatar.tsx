cb00cf7d4e422b7bac9379ad23ef0eeb275804fe
Santiago Andres Quintero Rodriguez <santianqr@gmail.com>
2024-03-08 14:35:25 -0500

    update

--- src/components/header-avatar.tsx
+++ src/components/header-avatar.tsx
@@ -1,22 +1,25 @@
 import { CircleUser, ChevronDown } from "lucide-react";
-import { getServerAuthSession } from "@/server/auth";
+//import { getServerAuthSession } from "@/server/auth";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useSession } from "next-auth/react";
 
 export async function HeaderAvatar() {
-  const session = await getServerAuthSession();
+  //const session = await getServerAuthSession();
+  const { data: session } = useSession();
+
   return (
     <div>
-        {session ? (
-          <Avatar className="ml-2 hidden h-[2rem] w-[2rem] sm:block">
-            <AvatarImage src="https://github.com/santianqr.png" alt="@shadcn" />
-            <AvatarFallback className="text-foreground">SQ</AvatarFallback>
-          </Avatar>
-        ) : (
-          <div className="flex items-center text-foreground/60">
-            <CircleUser />
-            <ChevronDown size={12} />
-          </div>
-        )}
+      {session ? (
+        <Avatar className="ml-2 hidden h-[2rem] w-[2rem] sm:block">
+          <AvatarImage src="https://github.com/santianqr.png" alt="@shadcn" />
+          <AvatarFallback className="text-foreground">SQ</AvatarFallback>
+        </Avatar>
+      ) : (
+        <div className="flex items-center text-foreground/60">
+          <CircleUser />
+          <ChevronDown size={12} />
+        </div>
+      )}
     </div>
   );
 }
